fbcbed33fb72fca4407702f8dba7ca347becc54d
reginaldodlneto <reginaldo.dlneto@gmail.com>
2021-09-12 11:12:55 -0300

    adicionando template default, header, user dashboard

---
+++ src/templates/Default.js
@@ -0,0 +1,13 @@
+import Header from '../components/Header'
+
+const Default = ({ children }) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <footer>FOOTER</footer>
+    </>
+  )
+}
+
+export default Default
