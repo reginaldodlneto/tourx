3d5bb3c2b0956bca09090561dac0705cc331cbb4
reginaldodlneto <reginaldo.dlneto@gmail.com>
2021-09-29 19:40:15 -0300

    adicionando login com google

--- pages/auth/signin/index.js
+++ pages/auth/signin/index.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { Formik } from 'formik'
 import axios from 'axios'
 import { useRouter } from 'next/router'
@@ -29,6 +30,11 @@ const Signin = () => {
 
   console.log(session)
 
+  const handleGoogleLogin = () =>
+    signIn('google', {
+      callbackUrl: 'http://localhost:3000/user/dashboard'
+    })
+
   const handleFormSubmit = async values => {
     signIn('credentials', {
       email: values.email,
@@ -52,6 +58,28 @@ const Signin = () => {
 
       <Container maxWidth="md">
         <Box className={classes.box}>
+          <Box display="flex" justifyContent="center">
+            <Button
+              variant="contained"
+              color="primary"
+              startIcon={
+                <Image
+                  src="/images/logo_google.svg"
+                  width={20}
+                  height={20}
+                  alt="Login com Google"
+                />
+              }
+              onClick={handleGoogleLogin}
+            >
+              Entrar com Google
+            </Button>
+          </Box>
+
+          <Box className={classes.orSeparator}>
+            <span>ou</span>
+          </Box>
+
           <Formik
             initialValues={initialValues}
             validationSchema={validationSchema}
